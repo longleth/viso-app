cb284061f488791681f2b1139e9e92c4797a076c
longleth <long.thanh.le@outlook.com>
2024-02-20 17:41:37 +0700

    Initial commit
    Signed-off-by: longleth <long.thanh.le@outlook.com>

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+#############################
+# VISO configuration server #
+#       Docker File         #
+#############################
+
+FROM        amazoncorretto:17.0.9-alpine
+LABEL       authors="longle"
+
+ADD         target/configserver-0.0.1-SNAPSHOT.jar /configserver-0.0.1-SNAPSHOT.jar
+
+ENTRYPOINT  ["java", "-jar", "configserver-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/startup/viso/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.startup.viso.configserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
